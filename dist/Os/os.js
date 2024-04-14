11121a74a4fd465be0313cb75a3a2e0478bc2d12
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-14 15:17:09 +0330

    Play around with os

--- dist/Os/os.js
+++ dist/Os/os.js
@@ -5,3 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const os_1 = __importDefault(require("os"));
 console.log(os_1.default.platform());
+console.log(os_1.default.arch());
+console.log(os_1.default.cpus());
+console.log(os_1.default.freemem());
+console.log(os_1.default.totalmem());
+console.log(os_1.default.homedir());
+console.log(os_1.default.uptime() / 60 / 60);
+console.log("Test after os");
