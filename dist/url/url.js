2baf97fbfb2b7760b07d145002bc9516821fcec9
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-14 15:26:40 +0330

    Play around with url

---
+++ dist/url/url.js
@@ -0,0 +1,12 @@
+"use strict";
+const myUrl = new URL("https://myWebsite.com/hello.html?id=106&status=active");
+console.log(myUrl.href);
+console.log(myUrl.toString());
+console.log(myUrl.host);
+console.log(myUrl.hostname);
+console.log(myUrl.pathname);
+console.log(myUrl.search);
+console.log(myUrl.searchParams);
+myUrl.searchParams.append("test", "abc");
+console.log(myUrl.searchParams);
+console.log(myUrl.searchParams.forEach((value, name) => console.log(name, value)));
